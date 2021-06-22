504817e491efac5aae102eaa64cc603f8d96341b
Owen <owen@howtank.com>
2021-06-22 12:23:30 +0200

    adding slider for mission

--- src/views/Mission.vue
+++ src/views/Mission.vue
@@ -1,21 +1,31 @@
 <template>
-    <div>
-            <div>
-            <GameStartScreen
-            v-bind:title="post.title"
-            v-bind:content="post.content"
-            v-bind:startScreen="post.backgroundImg"/></div>
-            <div><Question1/></div>
-            <div><Question2/></div>
-            <div><Question3/></div>
+    <div >
+        <Carousel :per-page="1" :mouse-drag="false">
+            <slide>
+                <GameStartScreen
+                v-bind:title="post.title"
+                v-bind:content="post.content"
+                v-bind:startScreen="post.backgroundImg"/>
+            </slide>
+            <slide>
+                <Question1/>
+            </slide>
+            <slide>
+                <Question2/>
+            </slide>
+            <slide>
+                <Question3/>
+            </slide>
+        </Carousel>
     </div>
 </template>
 <script>
+
 import GameStartScreen from '../components/gameUi/GameStartScreen.vue'
 import Question1 from '../components/gameUi/Question1.vue'
 import Question2 from '../components/gameUi/Question2.vue'
 import Question3 from '../components/gameUi/Question3.vue'
-
+import { Carousel, Slide } from 'vue-carousel';
 import axios from 'axios'
 export default({
     name: 'Home',
@@ -23,11 +33,13 @@ export default({
         GameStartScreen,
         Question1,
         Question2,
-        Question3
+        Question3,
+        Carousel,
+        Slide
     },
     data() {
         return {
-        mission:null
+        post:null
         }
     },
     setup() {
@@ -39,8 +51,7 @@ export default({
             const id = splitUrl[4]
             const res = await axios.get('http://localhost:6590/v1/mission/' +id, {
             })
-            this.mission = res.data.data
-            console.log(this.mission)
+            this.post = res.data.data
         },
         
     },
